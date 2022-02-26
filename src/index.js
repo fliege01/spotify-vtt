2e7e19cc1c707af9162772d2a4c0a0787cdc720b
Nico Bischof <nico@bischof.family>
2022-02-26 15:20:31 +0100

    Push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
+class SpotifyVttPlayer {
+    constructor() {
+        let d = new Dialog({
+            title: "Test Dialog",
+            content: "<p>You must choose either Option 1, or Option 2</p>",
+            buttons: {
+                one: {
+                    icon: '<i class="fas fa-check"></i>',
+                    label: "Option One",
+                    callback: () => console.log("Chose One")
+                },
+                two: {
+                    icon: '<i class="fas fa-times"></i>',
+                    label: "Option Two",
+                    callback: () => console.log("Chose Two")
+                }
+            },
+            default: "two",
+            render: html => console.log("Register interactivity in the rendered dialog"),
+            close: html => console.log("This always is logged no matter which option is chosen")
+        });
+        d.render(true);
+    }
+}
 
-Hooks.once('ready', () => {
-    if(!game.modules.get('lib-wrapper')?.active && game.user.isGM)
-        ui.notifications.error("Module XYZ requires the 'libWrapper' module. Please install and activate it.");
-    else ui.notifications.info("Spotify loaded.");
+
+Hooks.once('init', () => {
+    game.modules.get('my-module-name').api = new SpotifyVttPlayer();
+
+    Hooks.callAll('SpotifyVttPlayerReady', game.modules.get('my-module-name').api)
 });
-
