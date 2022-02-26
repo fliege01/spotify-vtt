64b88497c7f152c4012ddb6528c83dd3fe86436a
Nico Bischof <nico@bischof.family>
2022-02-26 13:37:00 +0100

    Push

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@
 Hooks.once('ready', () => {
     if(!game.modules.get('lib-wrapper')?.active && game.user.isGM)
         ui.notifications.error("Module XYZ requires the 'libWrapper' module. Please install and activate it.");
+    else ui.notifications.info("Spotify loaded.");
 });
 
